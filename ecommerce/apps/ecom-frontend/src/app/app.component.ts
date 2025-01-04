19cc15abeedb0c156f2aa185a9000a7cd7d99d44
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-04 16:09:26 -0300

    feat: Create a navbar component

--- ecommerce/apps/ecom-frontend/src/app/app.component.ts
+++ ecommerce/apps/ecom-frontend/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
+  standalone: true,
   imports: [ RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecom-frontend';
+export class AppComponent implements OnInit {
+
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
