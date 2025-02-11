3dfb0e2d7bdadaa221f072317f03f4f035b2b208
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-11 11:00:39 -0300

    Feat: Add dao layer with product features

---
+++ 02-backend/ecom/src/main/java/com/majesty/ecom/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.majesty.ecom.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.majesty.ecom.entity.Product;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
