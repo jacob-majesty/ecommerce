ac8f2a103389adb21d68665935bb92297870a2a1
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-13 10:21:04 -0300

    Feat: Search by product category

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -7,16 +7,26 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Product } from './common/product';
 import { ProductService } from './services/product.service';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+
+];
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [ 
     ProductService,
