7b589274864f188107985d5d93b7d9183f07e9a3
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-13 11:00:05 -0300

    Feat: Add FindByCategory in spring

--- 02-backend/ecom/src/main/java/com/majesty/ecom/dao/ProductRepository.java
+++ 02-backend/ecom/src/main/java/com/majesty/ecom/dao/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.majesty.ecom.dao;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.domain.Page;
 
 import com.majesty.ecom.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Page<Product> findByCategoryId(@Param("id") long id, Pageable pageable);
 }
