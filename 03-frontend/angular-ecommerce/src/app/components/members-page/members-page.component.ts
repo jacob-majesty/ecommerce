dbff7b5e5b291ec459f277d50ed472b4f2dab03d
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-23 18:37:31 -0300

    Feat: Add member page component

---
+++ 03-frontend/angular-ecommerce/src/app/components/members-page/members-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-members-page',
+  templateUrl: './members-page.component.html',
+  styleUrl: './members-page.component.css'
+})
+export class MembersPageComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
