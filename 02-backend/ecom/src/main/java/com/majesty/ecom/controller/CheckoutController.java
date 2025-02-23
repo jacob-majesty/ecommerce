9eb38da9a71c94f9a200599af9b355788b3e93ab
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-02-23 00:41:18 -0300

    Feat: Add checkout controller

---
+++ 02-backend/ecom/src/main/java/com/majesty/ecom/controller/CheckoutController.java
@@ -0,0 +1,32 @@
+package com.majesty.ecom.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.majesty.ecom.dto.Purchase;
+import com.majesty.ecom.dto.PurchaseResponse;
+import com.majesty.ecom.service.CheckoutService;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+}
