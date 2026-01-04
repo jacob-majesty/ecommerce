6076ec49b27653d7b882594443f6284b38d6814e
“[jacob-majesty]” <[jacobamony@gmail.com]>
2026-01-03 21:39:40 -0300

    feat(ui): implement product category menu

---
+++ frontend/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import {ProductCategory} from "../../common/product-category";
+import {ProductService} from "../../services/product.service";
+
+@Component({
+  selector: 'app-product-category-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './product-category-menu.component.html',
+  styleUrls: ['./product-category-menu.component.css']
+})
+export class ProductCategoryMenuComponent implements OnInit {
+
+  productCategories: ProductCategory[] = [];
+
+  private productService = inject(ProductService);
+
+  ngOnInit() {
+    this.listProductCategories();
+  }
+
+  listProductCategories() {
+    this.productService.getProductCategories().subscribe(
+      data => {
+        console.log('Product Categories=' + JSON.stringify(data));
+        this.productCategories = data;
+      }
+    );
+  }
+}
