427ba6a54f9feaa91de9426145f4fd955052cf0e
“[jacob-majesty]” <[jacobamony@gmail.com]>
2026-01-17 18:30:15 -0300

    feat(ui): add product details route

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient} from "@angular/common/http";
+import { provideHttpClient } from "@angular/common/http";
+import { CurrencyPipe } from "@angular/common";
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [
+    provideRouter(routes),
     provideHttpClient(),
+    CurrencyPipe
   ]
 };
