43afa0ae38ef2197962348c5e2f5977f1c725296
“[jacob-majesty]” <[jacobamony@gmail.com]>
2026-01-17 18:31:03 -0300

    feat(ui): add product list components

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -2,12 +2,12 @@ import {Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
