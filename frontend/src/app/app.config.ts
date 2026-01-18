69567616f6fd77b3086543a42eb58eb0ef7637cc
“[jacob-majesty]” <[jacobamony@gmail.com]>
2026-01-17 23:17:36 -0300

    feat(ui): implement paginated search and migrate product list to signals

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from "@angular/common/http";
 import { CurrencyPipe } from "@angular/common";
 
 import { routes } from './app.routes';
+import { NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    CurrencyPipe
+    CurrencyPipe,
+    importProvidersFrom(NgbPaginationModule)
   ]
 };
--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -38,6 +38,31 @@ export class ProductService {
     );
   }
 
+  getProductListPaginate(thePage: number,
+                         thePageSize: number,
+                         theCategoryId: number): Observable<GetResponseProducts> {
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  searchProductsPaginate(thePage: number,
+                         thePageSize: number,
+                         theKeyword: string): Observable<GetResponseProducts> {
+
+    // build URL based on keyword, page and size
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      catchError(this.handleError)
+    );
+  }
+
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded?.products ?? []),
@@ -56,6 +81,12 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  };
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   };
 }
 
