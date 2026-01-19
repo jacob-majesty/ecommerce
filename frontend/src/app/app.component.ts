480292c20228bebd4e10e59a51b8ab47634b6968
Jacob <161256430+jacob-majesty@users.noreply.github.com>
2026-01-19 02:02:18 -0300

    feat(ui): implement shopping cart logic (#9)
    * feat(ui): implement shopping cart logic and product detail integration
    
    * feat(ui): complete shopping cart feature with quantity controls and totals

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import {ProductCategoryMenuComponent} from "./components/product-category-menu/product-category-menu.component";
 import {SearchComponent} from "./components/search/search.component";
+import {CartStatusComponent} from "./components/cart-status/cart-status.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent, ProductCategoryMenuComponent, SearchComponent],
+  imports: [RouterOutlet, ProductListComponent, ProductCategoryMenuComponent, SearchComponent, CartStatusComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/components/cart-status/cart-status.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {CartService} from "../../services/cart.service";
+import {CurrencyPipe} from "@angular/common";
+import {RouterLink} from "@angular/router";
+
+
+@Component({
+  selector: 'app-cart-status',
+  templateUrl: './cart-status.component.html',
+  standalone: true,
+  imports: [
+    CurrencyPipe,
+    RouterLink
+  ],
+  styleUrls: ['./cart-status.component.css']
+})
+export class CartStatusComponent implements OnInit {
+
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+  }
+}
