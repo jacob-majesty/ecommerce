824e909412004118e6fe0770d7c986736271a4a0
“[jacob-majesty]” <[jacobamony@gmail.com]>
2026-01-26 01:39:38 -0300

    feat: add country and state repositories

---
+++ backend/src/main/java/com/majesty/ecommerce/dao/CountryRepository.java
@@ -0,0 +1,11 @@
+package com.majesty.ecommerce.dao;
+
+import com.majesty.ecommerce.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:80")
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+}
