eaa8f40cf4c7017a147486953b3863397695b28e
“[jacob-majesty]” <[jacobamony@gmail.com]>
2026-02-17 19:20:01 -0300

    feat(ui): implement vip members access

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 import {ProductListComponent} from "./components/product-list/product-list.component";
 import {ProductDetailsComponent} from "./components/product-details/product-details.component";
 import {CartDetailsComponent} from "./components/cart-details/cart-details.component";
 import {CheckoutComponent} from "./components/checkout/checkout.component";
 import { LoginStatusComponent } from './components/login-status/login-status.component';
+import {MembersPageComponent} from "./components/members-page/members-page.component";
 
 export const routes: Routes = [
+  {path: 'members', component: MembersPageComponent, canActivate: [AuthGuard]},
   {path: 'checkout', component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
