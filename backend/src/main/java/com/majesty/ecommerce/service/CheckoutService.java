4dab3143cf33a1f656b014a197a8f4c32097e309
“[jacob-majesty]” <[jacobamony@gmail.com]>
2026-02-21 22:00:06 -0300

    feat: add stripe to checkout service

--- backend/src/main/java/com/majesty/ecommerce/service/CheckoutService.java
+++ backend/src/main/java/com/majesty/ecommerce/service/CheckoutService.java
@@ -1,9 +1,13 @@
 package com.majesty.ecommerce.service;
 
+import com.majesty.ecommerce.dto.PaymentInfo;
 import com.majesty.ecommerce.dto.Purchase;
 import com.majesty.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
